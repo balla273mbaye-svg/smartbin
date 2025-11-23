978953640d2a67c211ae1a9b53c0d397d2a4ba1e
apple <apple@apples-MacBook-Air-2.local>
2025-11-23 23:49:50 +0000

    FORCE: Fix settings.py syntax error

--- smartbin/settings.py
+++ smartbin/settings.py
@@ -1,16 +1,9 @@
-cd /Users/apple/smartbin
-
-# Remplacer complètement le fichier
-cat > smartbin/settings.py << 'EOF'
 from pathlib import Path
 import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 SECRET_KEY = "django-insecure-#54y2+ac$bj*xyt1#zj(kp)x39%9-0w1(ik7jgq)gove9%tc(t"
-
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
-
 ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
@@ -36,22 +29,20 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "smartbin.urls"
 
-TEMPLATES = [
-    {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        'DIRS': [BASE_DIR / "templates"], 
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.debug",
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
-                "django.template.context_processors.media",
-            ],
-        },
+TEMPLATES = [{
+    "BACKEND": "django.template.backends.django.DjangoTemplates",
+    'DIRS': [BASE_DIR / "templates"], 
+    "APP_DIRS": True,
+    "OPTIONS": {
+        "context_processors": [
+            "django.template.context_processors.debug",
+            "django.template.context_processors.request",
+            "django.contrib.auth.context_processors.auth",
+            "django.contrib.messages.context_processors.messages",
+            "django.template.context_processors.media",
+        ],
     },
-]
+}]
 
 WSGI_APPLICATION = "smartbin.wsgi.application"
 
@@ -73,26 +64,11 @@ LANGUAGE_CODE = "fr-fr"
 TIME_ZONE = "UTC"
 USE_I18N = True
 USE_TZ = True
-
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
 FILE_UPLOAD_MAX_MEMORY_SIZE = 104857600
 DATA_UPLOAD_MAX_MEMORY_SIZE = 104857600
-EOF
-
-# Vérifier que le fichier est correct
-cat smartbin/settings.py | grep "ALLOWED_HOSTS"
-
-# Doit afficher : ALLOWED_HOSTS = ['*']
-
-# Push
-git add smartbin/settings.py
-git commit -m "Complete rewrite of settings.py - fix syntax"
-git push origin main
